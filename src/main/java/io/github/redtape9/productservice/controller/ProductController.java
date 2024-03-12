5ccd18aad905016b5e2afe3de3d12e1d75366a24
Sergej Jaudszims <redtape9688@gmail.com>
2024-03-12 13:42:24 +0100

    add createProduct method with log

--- src/main/java/io/github/redtape9/productservice/controller/ProductController.java
+++ src/main/java/io/github/redtape9/productservice/controller/ProductController.java
@@ -1,9 +1,22 @@
 package io.github.redtape9.productservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.redtape9.productservice.dto.ProductRequest;
+import io.github.redtape9.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/product")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+
+    }
 }
---
+++ src/main/java/io/github/redtape9/productservice/service/ProductService.java
@@ -0,0 +1,28 @@
+package io.github.redtape9.productservice.service;
+
+import io.github.redtape9.productservice.dto.ProductRequest;
+import io.github.redtape9.productservice.model.Product;
+import io.github.redtape9.productservice.repo.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product with Id: {} created ", product.getId());
+    }
+
+}
