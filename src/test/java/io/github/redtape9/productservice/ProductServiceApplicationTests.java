a61a390b460a363df82cecbe65fd53202b891821
Sergej Jaudszims <redtape9688@gmail.com>
2024-03-12 13:06:43 +0100

    Initial commit

---
+++ src/test/java/io/github/redtape9/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.redtape9.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
